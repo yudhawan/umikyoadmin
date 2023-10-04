5ab8b8491bc2620ea7e8466617a4cc1742e24c7a
yudhawan <yudhaashter@gmail.com>
2023-10-04 08:37:41 +0700

    test prisma

--- src/app/api/testData/route.ts
+++ src/app/api/testData/route.ts
@@ -16,10 +16,8 @@ export type BodyProp = {
 // }
 export const GET = async (request: NextRequest) => {
     try {
-        // const { category, sub }: BodyProp = await request.json()
-        // if (category) await prisma.categories.create({ data: { category: category } })
-        // if (sub) await prisma.subCategory.create({ data: { sub: sub } })
-        return NextResponse.json('created', { status: 202 })
+        const test = await prisma.categories.findMany()
+        return NextResponse.json({ categories: test }, { status: 202 })
     } catch (error) {
         return NextResponse.json({ message: "POST error: ", error }, { status: 500 })
     }
