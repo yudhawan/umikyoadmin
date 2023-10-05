256ef8d2665c144bb8c1be17082671cf90affb1e
yudhawan <yudhaashter@gmail.com>
2023-10-05 15:17:00 +0700

    das

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,11 @@
 import { NextResponse } from "next/server";
-const allowOrigin = process.env.NODE_ENV === 'production' ? [process.env.BASE_URL, 'https://umikyo.vercel.app/'] : ['http://localhost:3000']
-export function middleware(request: Request) {
+
+export function middleware() {
     const res = NextResponse.next()
-    const origin = request.headers.get('origin')
-    if (origin && !allowOrigin.includes(origin)) return new NextResponse(null, {
-        status: 400,
-        headers: {
-            'Content-Type': 'text/plain'
-        }
-    })
+
     // add the CORS headers to the response
     res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', `${allowOrigin}`) // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Origin', 'http://localhost:3000') // replace this your actual origin
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
     res.headers.append(
         'Access-Control-Allow-Headers',
