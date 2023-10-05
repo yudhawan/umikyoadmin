27bf9b5573c859b82bf0cbabd5e37a0f9439324a
yudhawan <yudhaashter@gmail.com>
2023-10-05 15:12:56 +0700

    test mdware

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export function middleware(request: Request) {
     })
     // add the CORS headers to the response
     res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Origin', `${allowOrigin}`) // replace this your actual origin
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
     res.headers.append(
         'Access-Control-Allow-Headers',
