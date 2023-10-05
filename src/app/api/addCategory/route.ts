cea18ca5d8ef27588003538ebed85ab0fc7972a0
yudhawan <yudhaashter@gmail.com>
2023-10-05 15:08:06 +0700

    middleware

--- src/app/api/addCategory/route.ts
+++ src/app/api/addCategory/route.ts
@@ -6,10 +6,16 @@ export type BodyProp = {
 }
 export const POST = async (request: NextRequest) => {
     try {
+        const origin = request.headers.get('origin')
         const { category, sub }: BodyProp = await request.json()
         if (category) await prisma.categories.create({ data: { category: category } })
         if (sub) await prisma.subCategory.create({ data: { sub: sub } })
-        return NextResponse.json('created', { status: 202 })
+        return new NextResponse('created', {
+            headers: {
+                'Access-Control-Allow-Origin': origin || "*",
+                'Content-Type': 'application/json'
+            }
+        })
     } catch (error) {
         return NextResponse.json({ message: "POST error: ", error }, { status: 500 })
     }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
-
-export function middleware() {
+const allowOrigin = process.env.NODE_ENV === 'production' ? [process.env.BASE_URL, 'https://umikyo.vercel.app/'] : ['http://localhost:3000']
+export function middleware(request: Request) {
     const res = NextResponse.next()
-
+    const origin = request.headers.get('origin')
+    if (origin && !allowOrigin.includes(origin)) return new NextResponse(null, {
+        status: 400,
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
     // add the CORS headers to the response
     res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', 'http://localhost:3000') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
     res.headers.append(
         'Access-Control-Allow-Headers',
